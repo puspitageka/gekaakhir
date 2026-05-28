10c1d6b790bfd6070a5d159454aa930ecc67e470
puspitageka <puspitageka@gmail.com>
2026-05-28 13:43:12 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,79 @@
-console.log("Website Coffee Shop Ready!");
+const slider =
+document.querySelector(".slider");
 
-window.addEventListener("scroll", () => {
-  console.log("Scrolling...");
+// NEXT
+document.querySelector(".next")
+.addEventListener("click", () => {
+
+  slider.scrollBy({
+    left:400,
+    behavior:"smooth"
+  });
+
 });
+
+// PREV
+document.querySelector(".prev")
+.addEventListener("click", () => {
+
+  slider.scrollBy({
+    left:-400,
+    behavior:"smooth"
+  });
+
+});
+
+// SCROLL
+function scrollPortfolio(){
+
+  document.querySelector("#portfolio")
+  .scrollIntoView({
+    behavior:"smooth"
+  });
+
+}
+
+// LOGIN LOGOUT
+
+const authArea =
+document.getElementById("authArea");
+
+const currentUser =
+localStorage.getItem("loggedInUser");
+
+if(currentUser){
+
+  authArea.innerHTML = `
+
+    <div class="user-box">
+
+      <span>
+        Hi, ${currentUser}
+      </span>
+
+      <button
+      onclick="logout()"
+      class="logout-btn">
+
+        Logout
+
+      </button>
+
+    </div>
+
+  `;
+
+}
+
+// LOGOUT
+function logout(){
+
+  localStorage.removeItem(
+    "loggedInUser"
+  );
+
+  alert("Yey, logout berhasil!");
+
+  window.location.reload();
+
+}
